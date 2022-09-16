a6303ad6751cfb3adaa3add9287bbb2dabfea2b6
Flavio <Flavio>
2022-09-17 00:27:48 +0200

    Init project

---
+++ app/src/main/java/it/fdepedis/myschooldomusapp/MainActivity.java
@@ -0,0 +1,106 @@
+package it.fdepedis.myschooldomusapp;
+
+import android.app.DownloadManager;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.CookieManager;
+import android.webkit.DownloadListener;
+import android.webkit.URLUtil;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private WebView webView;
+    private final String URL = "https://webscuola.scuoladivinaprovvidenza.it";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webView);
+
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        // Configure the client to use when opening URLs
+        webView.setWebViewClient(new WebViewClient());
+
+        // Load URL
+        webView.loadUrl(URL);
+
+        // Enable responsive layout
+        webView.getSettings().setUseWideViewPort(true);
+        // Zoom out if the content width is greater than the width of the viewport
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setSupportZoom(true);
+        webView.getSettings().setBuiltInZoomControls(true); // allow pinch to zooom
+        webView.getSettings().setDisplayZoomControls(false); // disable the default zoom controls on the page
+        webView.getSettings().setDomStorageEnabled(true); // load property javascript
+        webView.getSettings().setAppCachePath(getBaseContext().getCacheDir().getAbsolutePath());
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.setDownloadListener(new DownloadListener() {
+            public void onDownloadStart(String url, String userAgent,
+                                        String contentDisposition, String mimeType,
+                                        long contentLength) {
+                DownloadManager.Request request = new DownloadManager.Request(
+                        Uri.parse(url));
+                request.setMimeType(mimeType);
+                String cookies = CookieManager.getInstance().getCookie(url);
+                request.addRequestHeader("cookie", cookies);
+                request.addRequestHeader("User-Agent", userAgent);
+                request.setDescription("Downloading File...");
+                request.setTitle(URLUtil.guessFileName(url, contentDisposition, mimeType));
+                request.allowScanningByMediaScanner();
+                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                request.setDestinationInExternalPublicDir(
+                        Environment.DIRECTORY_DOWNLOADS, URLUtil.guessFileName(
+                                url, contentDisposition, mimeType));
+                DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+                dm.enqueue(request);
+                Toast.makeText(getApplicationContext(), "Downloading File", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed(){
+        WebView wv = (WebView)findViewById(R.id.webView);
+        if(wv.canGoBack()){
+            wv.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+
+            webView.loadUrl(URL);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
